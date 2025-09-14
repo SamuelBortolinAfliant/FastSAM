84df2493c63fdc720208b04786ff317789dba8b9
Davide Frageri <davide.frag@gmail.com>
2025-09-14 21:30:16 +0200

    fix local package ultralytics in setup.py file

--- setup.py
+++ setup.py
@@ -12,10 +12,11 @@ setup(
     name="fastsam",
     version="0.1.1",
     install_requires=REQUIREMENTS,
-    packages=["fastsam", "fastsam_tools"],
+    packages=["fastsam", "fastsam_tools", "ultralytics"],
     package_dir= {
         "fastsam": "fastsam",
         "fastsam_tools": "utils",
+        "ultralytics": "ultralytics"
     },
     url="https://github.com/CASIA-IVA-Lab/FastSAM"
 )
